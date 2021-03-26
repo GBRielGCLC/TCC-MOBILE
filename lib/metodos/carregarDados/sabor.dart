2bcf3bfcb5a0a5758f8bf6689a003ee5e76abc20
GBRielGCLC <gabriel.gclc@gmail.com>
2021-03-26 19:08:26 -0300

    sabor carregado no dialog, falta deixar bonito

--- lib/metodos/carregarDados/sabor.dart
+++ lib/metodos/carregarDados/sabor.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:spinner_input/spinner_input.dart';
 
 carregarSabor() async{
   var url="https://pizzaria-do-careca.000webhostapp.com/dadosSabor.php";//Link do arquivo que carrega os dados
@@ -28,9 +29,22 @@ sabor(){
         return ListView.builder(
           itemCount: sabor.length,
           itemBuilder: (context, index){
-            return ListTile(
-              title: Text("${sabor[index]['nome']}"),
-              subtitle: Text("R\$ ${sabor[index]['precoAdd']}"),
+            double qtde=0;
+            return StatefulBuilder(
+              builder: (context, setState){
+                return ListTile(
+                  title: Text("${sabor[index]['nome']}"),
+                  subtitle: Text("${sabor[index]['descricao']}"),
+                  trailing: SpinnerInput(
+                    spinnerValue: qtde,
+                    onChange: (newValue){
+                      setState((){
+                        qtde = newValue;
+                      });
+                    },
+                  ),
+                );
+              }
             );
           },
         );
--- lib/metodos/carregarDados/tamanho.dart
+++ lib/metodos/carregarDados/tamanho.dart
@@ -1,6 +1,8 @@
+import 'package:pizzaria/metodos/carregarDados/sabor.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:spinner_input/spinner_input.dart';
 
 carregarTamanho() async{
   var url="https://pizzaria-do-careca.000webhostapp.com/dadosTamanho.php";//Link do arquivo que carrega os dados
@@ -11,47 +13,93 @@ carregarTamanho() async{
 }
 tamanho(){
   return FutureBuilder(
-      future: carregarTamanho(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        List tamanho = snapshot.data;
-        if(snapshot.connectionState==ConnectionState.waiting){
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if(snapshot.hasError){
-          return Center(
-            child: Text("Erro"),
-          );
-
-        }
-        return ListView.builder(
-          itemCount: tamanho.length,
-          itemBuilder: (context, index){
-            return Column(
-              children: [
-                SizedBox(height: 10.0),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  child: Card(
-                    child: ListTile(
-                      title: Text("${tamanho[index]['nome']}"),
-                      subtitle: Text("R\$ ${tamanho[index]['preco']}"),
-                      trailing: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.add_shopping_cart),
-                        iconSize: 30,
-                      ),
-                    ),
-                    elevation: 3,
-                    color: Colors.grey[200],
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  ),
-                ),
-              ],
-            );
-          },
+    future: carregarTamanho(),
+    builder: (BuildContext context, AsyncSnapshot snapshot) {
+      List tamanho = snapshot.data;
+      if(snapshot.connectionState==ConnectionState.waiting){
+        return Center(
+          child: CircularProgressIndicator(),
         );
-      },
-    );
+      }
+      if(snapshot.hasError){
+        return Center(
+          child: Text("Erro"),
+        );
+
+      }
+      return ListView.builder(
+        itemCount: tamanho.length,
+        itemBuilder: (context, index){
+          return Column(
+            children: [
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Card(
+                  child: ListTile(
+                    onTap: (){
+                      dialogSabor(context, tamanho, index);
+                    },
+                    title: Text("${tamanho[index]['nome']}"),
+                    subtitle: Text("R\$ ${tamanho[index]['preco']}"),
+                    trailing: IconButton(
+                      onPressed: (){
+                        dialogSabor(context,tamanho, index);
+                      },
+                      icon: Icon(Icons.add_shopping_cart),
+                      iconSize: 30,
+                    ),
+                  ),
+                  elevation: 3,
+                  color: Colors.grey[200],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );
+}
+
+dialogSabor(BuildContext context, tamanho, index) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return StatefulBuilder(
+        builder: (context, setState){
+          return AlertDialog(
+            title: Text("${tamanho[index]['nome']}"),
+            content: sabor(),
+            actions: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RaisedButton(
+                      child: Text("Fechar"),
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      color: Colors.red,
+                    ),
+                    RaisedButton(
+                      child: Text("Adicionar"),
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );
 }
